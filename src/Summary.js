6f22bd608f424b72bab1946350cb01a62c78e3a0
raheeeg <raheegibrahim@gmail.com>
2020-12-28 21:53:41 +0000

    Style
    Style edited

--- src/Summary.js
+++ src/Summary.js
@@ -3,28 +3,28 @@ import "./Summary.css";
 
 export default function Summary() {
     return (
-        <div className="Summary card">
-                <div className="Summary card-body">
-      <div class="row">
-        <div class="summary-text temp-low col-9">
+    <div className="Summary card">
+      <div className="Summary card-body">
+        <div className="row">
+        <div className="summary-text temp-low col-8">
           <span>4</span>
         </div>
-        <div class="summary-text temp-current col-3">
-          <span class="temperature">5</span>
-          <span class="unit float-right">
-            <a href="#" class="active">
+        <div className="summary-text temp-current col-4">
+          <span className="temperature">5</span>
+          <span className="unit float-right">
+            <a href="#" className="active">
               °C
             </a>
             |<a href="#">°F</a>
           </span>
         </div>
-        <div class="summary-text temp-high col-9">
+        <div className="summary-text temp-high col-8">
           <span id="temp-high">6</span>
         </div>
-        <div class="summary-text current-city col-6">Paris</div>
-        <div class="summary-text current-date-time col-6">Sunday 27, 19:31</div>
+        <div className="summary-text current-city col-6">Paris</div>
+        <div className="summary-text current-date-time col-6">Sunday 27, 19:31</div>
       </div>
     </div>
-        </div>
+    </div>
     )
 }
